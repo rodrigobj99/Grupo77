76bd3b8dc862287f6b9b75c6c314fd60e369595d
Cristian <cristian.infante@uc.cl>
2020-09-29 19:39:50 -0300

    m

--- Sites/consultas/jefes_por_puertos.php
+++ Sites/consultas/jefes_por_puertos.php
@@ -7,7 +7,7 @@
 
   $nombre = $_POST["nombre_elegido"];
   //      select psnombre from personal, instalaciones, puertos where personal.inid = instalaciones.inid and personal.psesjefe = 'si' and puertos.puid = instalaciones.puid and puertos.punombre = 'Mejillones';
- 	$query = "SELECT psnombre FROM personal, instalaciones, puertos WHERE personal.inid = instalciones.inid AND personal.psesjefe = 'si' AND puertos.puid = instalaciones.puid AND puertos.punombre LIKE '%$nombre%';";
+ 	$query = "SELECT psnombre FROM personal, instalaciones, puertos WHERE personal.inid = instalciones.inid AND personal.psesjefe = 'si' AND puertos.puid = instalaciones.puid AND puertos.punombre = 'Mejillones';";
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$pokemones = $result -> fetchAll();
