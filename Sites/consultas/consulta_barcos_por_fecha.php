592c51f4ef42b42fd4fcb55d9d135635a2f1d771
Cristian <cristian.infante@uc.cl>
2020-09-30 14:27:56 -0300

    s

--- Sites/consultas/consulta_barcos_por_fecha.php
+++ Sites/consultas/consulta_barcos_por_fecha.php
@@ -5,9 +5,9 @@
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
 
-  $puerto = $_POST["puerto"];
+  $fecha = $_POST["fecha"];
 
- 	$query = "SELECT puertos.punombre, COUNT(barcos.bpatente) as llegadas FROM PERMISOS, BARCOS, INSTALACIONES, PUERTOS WHERE permisos.inid = instalaciones.inid AND instalaciones.puid = puertos.puid AND barcos.bpatente = permisos.bpatente AND permisos.pmatraque >= '2020-08-01' AND permisos.pmatraque <= '2020-08-31' GROUP BY (puertos.puid) HAVING COUNT(barcos.bpatente) = (SELECT MAX(llegadas) FROM (SELECT puertos.punombre, COUNT(barcos.bpatente) as llegadas FROM PERMISOS, BARCOS, INSTALACIONES, PUERTOS WHERE permisos.inid = instalaciones.inid AND instalaciones.puid = puertos.puid AND barcos.bpatente = permisos.bpatente AND permisos.pmatraque >= '2020-08-01' AND permisos.pmatraque <= '2020-08-31' GROUP BY (puertos.puid)) as FOO);";
+ 	$query = "SELECT puertos.punombre, COUNT(barcos.bpatente) as llegadas FROM PERMISOS, BARCOS, INSTALACIONES, PUERTOS WHERE permisos.inid = instalaciones.inid AND instalaciones.puid = puertos.puid AND barcos.bpatente = permisos.bpatente AND permisos.pmatraque >= '2020-'.'08'.'-01' AND permisos.pmatraque <= '2020-08-31' GROUP BY (puertos.puid) HAVING COUNT(barcos.bpatente) = (SELECT MAX(llegadas) FROM (SELECT puertos.punombre, COUNT(barcos.bpatente) as llegadas FROM PERMISOS, BARCOS, INSTALACIONES, PUERTOS WHERE permisos.inid = instalaciones.inid AND instalaciones.puid = puertos.puid AND barcos.bpatente = permisos.bpatente AND permisos.pmatraque >= '2020-08-01' AND permisos.pmatraque <= '2020-08-31' GROUP BY (puertos.puid)) as FOO);";
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$pokemones = $result -> fetchAll();
